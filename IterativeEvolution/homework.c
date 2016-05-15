37caf42aff977ce34085fb6d358491f6ec159972
gennartan <antoine.gennart@student.uclouvain.be>
2016-05-15 09:59:27 +0200

    iterative Solver : début de l'implémentation

--- IterativeEvolution/homework.c
+++ IterativeEvolution/homework.c
@@ -52,24 +52,16 @@ typedef struct {
 
 } femDiscrete;
 
-typedef struct {
-    femSolverType type;
-    void *solver;
+typedef struct { // ceci est un solveur itératif
+	double *R;
+	double *R0;
+	double *D;
+	double *D0;
+	// ON pourra supprimer une de ces quatres variables (voir énoncé APE)
+	double error;
+	int size;
+	int iter;
 } femSolver;
-
-typedef struct {
-    double *B;
-    double **A;
-    int size;
-} femFullSystem;
-
-typedef struct 
-{
-    double *B;
-    double **A;        
-    int size;
-    int band;        
-} femBandSystem;
 
 typedef struct {
 	femMesh *mesh;
@@ -116,38 +108,7 @@ void                 femDiscretePhi1(femDiscrete* mySpace, double xsi, double *p
 void                 femDiscreteDphi1(femDiscrete* mySpace, double xsi, double *dphidxsi);
 double               femDiscreteInterpolate(double *phi, double *U, int *map, int n);
 
-femSolver*           femSolverFullCreate(int size);
-femSolver*           femSolverBandCreate(int size,int band);
-femSolver*           femSolverIterativeCreate(int size);
-void                 femSolverFree(femSolver* mySolver);
-void                 femSolverInit(femSolver* mySolver);
-void                 femSolverPrint(femSolver* mySolver);
-void                 femSolverPrintInfos(femSolver* mySolver);
-double*              femSolverEliminate(femSolver* mySolver);
-void                 femSolverConstrain(femSolver* mySolver, int myNode, double value);
-void                 femSolverAssemble(femSolver* mySolver, double *Aloc, double *Bloc, double *Uloc, int *map, int nLoc);
-double               femSolverGet(femSolver* mySolver, int i, int j);
-int                  femSolverConverged(femSolver *mySolver);
-
-femFullSystem*       femFullSystemCreate(int size);
-void                 femFullSystemFree(femFullSystem* mySystem);
-void                 femFullSystemInit(femFullSystem* mySystem);
-void                 femFullSystemPrint(femFullSystem* mySystem);
-void                 femFullSystemPrintInfos(femFullSystem* mySystem);
-double*              femFullSystemEliminate(femFullSystem* mySystem);
-void                 femFullSystemConstrain(femFullSystem* mySystem, int myNode, double value);
-void                 femFullSystemAssemble(femFullSystem* mySystem, double *Aloc, double *Bloc, int *map, int nLoc);
-double               femFullSystemGet(femFullSystem* mySystem, int i, int j);
-
-femBandSystem*       femBandSystemCreate(int size, int band);
-void                 femBandSystemFree(femBandSystem* myBandSystem);
-void                 femBandSystemInit(femBandSystem *myBand);
-void                 femBandSystemPrint(femBandSystem *myBand);
-void                 femBandSystemPrintInfos(femBandSystem *myBand);
-double*              femBandSystemEliminate(femBandSystem *myBand);
-void                 femBandSystemConstrain(femBandSystem *myBand, int myNode, double myValue);
-void                 femBandSystemAssemble(femBandSystem* myBandSystem, double *Aloc, double *Bloc, int *map, int nLoc);
-double               femBandSystemGet(femBandSystem* myBandSystem, int i, int j);
+// .h du femsolver // TODO
 
 void                 femError(char *text, int line, char *file);
 void                 femWarning(char *text, int line, char *file);
@@ -793,360 +754,63 @@ double femDiscreteInterpolate(double *phi, double *U, int *map, int n)
 
 ////// FEMSOLVER ///////
 
-femSolver *femSolverFullCreate(int size)
-{
-    femSolver *mySolver = malloc(sizeof(femSolver));
-    mySolver->type = FEM_FULL;
-    mySolver->solver = (femSolver *)femFullSystemCreate(size);
-    return(mySolver);
+femSolver *femSolverCreate(int size){
+	femSolver *mySolver = malloc(sizeof(femSolver));
+	mySolver->R  = malloc(sizeof(double)*size*4);
+	mySolver->R0 = mySolver->R + size;
+	mySolver->D  = mySolver->R + size*2;
+	mySolver->D0 = mySolver->R + size*3;
+	mySolver->size = size;
+	femSolverInit(mySolver);
+	return(mySolver);
 }
 
-
-void femSolverFree(femSolver *mySolver)
-{
-    switch (mySolver->type) {
-        case FEM_FULL : femFullSystemFree((femFullSystem *)mySolver->solver); break;
-        case FEM_BAND : femBandSystemFree((femBandSystem *)mySolver->solver); break;
-        default : Error("Unexpected solver type"); }
-    free(mySolver);
+void femSolverFree(femSolver *mySolver){
+	free(mySolver->R);
+	free(mySolver);
 }
 
-void femSolverInit(femSolver *mySolver)
-{
-    switch (mySolver->type) {
-        case FEM_FULL : femFullSystemInit((femFullSystem *)mySolver->solver); break;
-        case FEM_BAND : femBandSystemInit((femBandSystem *)mySolver->solver); break;
-        default : Error("Unexpected solver type"); }
+void femSolverInit(femSolver *mySolver){
+	int i;
+	mySolver->iter = 0;
+	mySolver->error = 10.0e+12;
+	for(i=0;i<mySolver->size*4;++i){
+		mySolver->R[i] = 0;
+	}
 }
 
-
-double femSolverGet(femSolver *mySolver,int i,int j)
-{
-    double value = 0;
-    switch (mySolver->type) {
-        case FEM_FULL : value = femFullSystemGet((femFullSystem *)mySolver->solver,i,j); break;
-        case FEM_BAND : value = femBandSystemGet((femBandSystem *)mySolver->solver,i,j); break;
-        default : Error("Unexpected solver type"); }
-    return(value);
+void femSolverAssemble(femSolver *mySolver, double *Aloc, double *Bloc, double *Uloc, int *map, int nLoc){
+	int i,j,myRow;
+	if(mySolver->iter==0){
+		for(i=0;i<nLoc;++i){
+			myRow = map[i];
+			mySolver->R[myRow] += Bloc[i];
+			mySolver->D[myRow] += Bloc[i];
+			for(j=0;j<nLoc;++j){
+				mySolver->R[myRow] -= Aloc[i*nLoc+j]*Uloc[j];
+				mySolver->D[myRow] -= Aloc[i*nLoc+j]*Uloc[j];
+			}
+		}
+	}else{
+		for(i=0;i<nLoc;++i){
+			myRow = map[i];
+			for(j=0;j<nLoc;j++){
+				int mycol = map[j];
+				mySolver->D0[myRow] += Alco[i*nLoc+j] * mySolver->D[myCol];
+			}
+		}
+	}
 }
 
-void femSolverPrint(femSolver *mySolver)
-{
-    switch (mySolver->type) {
-        case FEM_FULL : femFullSystemPrint((femFullSystem *)mySolver->solver); break;
-        case FEM_BAND : femBandSystemPrint((femBandSystem *)mySolver->solver); break;
-        default : Error("Unexpected solver type"); }
+void femSovlerConstrain(femSolver *mySolver, int myNode, double myValue){
+	mySolver->R[myNode] = myValue;
+	mySolver->D0[myNode] = myValue;
+	mySolver->D[myNode] = myValue;
 }
 
-void femSolverPrintInfos(femSolver *mySolver)
-{
-    switch (mySolver->type) {
-        case FEM_FULL : femFullSystemPrintInfos((femFullSystem *)mySolver->solver); break;
-        case FEM_BAND : femBandSystemPrintInfos((femBandSystem *)mySolver->solver); break;
-        default : Error("Unexpected solver type"); }
+double *femSolverEliminate(femSolver *mySolver){
+
 }
-
-  
-void femSolverAssemble(femSolver* mySolver, double *Aloc, double *Bloc, double *Uloc,int *map, int nLoc)
-{
-    switch (mySolver->type) {
-        case FEM_FULL : femFullSystemAssemble((femFullSystem *)mySolver->solver,Aloc,Bloc,map,nLoc); break;
-        case FEM_BAND : femBandSystemAssemble((femBandSystem *)mySolver->solver,Aloc,Bloc,map,nLoc); break;
-        default : Error("Unexpected solver type"); }
-}
-    
-void femSolverConstrain(femSolver *mySolver, int myNode, double myValue)
-{
-    switch (mySolver->type) {
-        case FEM_FULL : femFullSystemConstrain((femFullSystem *)mySolver->solver,myNode,myValue); break;
-        case FEM_BAND : femBandSystemConstrain((femBandSystem *)mySolver->solver,myNode,myValue); break;
-        default : Error("Unexpected solver type"); }
-}
-  
-double *femSolverEliminate(femSolver *mySolver)
-{
-    double *soluce;
-    switch (mySolver->type) {
-        case FEM_FULL : soluce = femFullSystemEliminate((femFullSystem *)mySolver->solver); break;
-        case FEM_BAND : soluce = femBandSystemEliminate((femBandSystem *)mySolver->solver); break;
-        default : Error("Unexpected solver type"); }
-    return(soluce);
-}
-
-
-
-int femSolverConverged(femSolver *mySolver)
-{
-    int  testConvergence;
-    switch (mySolver->type) {
-        case FEM_FULL : testConvergence = 1; break;
-        case FEM_BAND : testConvergence = 1; break;
-        default : Error("Unexpected solver type"); }
-    return(testConvergence);
-}
-
-
-femFullSystem *femFullSystemCreate(int size)
-{
-    femFullSystem *theSystem = malloc(sizeof(femFullSystem));
-    theSystem->A = malloc(sizeof(double*) * size); 
-    theSystem->B = malloc(sizeof(double) * size * (size+1));
-    theSystem->A[0] = theSystem->B + size;  
-    theSystem->size = size;
-    int i;
-    for (i=1 ; i < size ; i++) 
-        theSystem->A[i] = theSystem->A[i-1] + size;
-    femFullSystemInit(theSystem);
-
-    return theSystem; 
-}
-
-void femFullSystemFree(femFullSystem *theSystem)
-{
-    free(theSystem->A);
-    free(theSystem->B);
-    free(theSystem);
-}
-
-
-void femFullSystemInit(femFullSystem *mySystem)
-{
-    int i,size = mySystem->size;
-    for (i=0 ; i < size*(size+1) ; i++) 
-        mySystem->B[i] = 0;}
-
-
-void femFullSystemPrint(femFullSystem *mySystem)
-{
-    double  **A, *B;
-    int     i, j, size;
-    A    = mySystem->A;
-    B    = mySystem->B;
-    size = mySystem->size;
-    
-    for (i=0; i < size; i++) {
-        for (j=0; j < size; j++)
-            if (A[i][j] == 0)  printf("         ");   
-            else               printf(" %+.1e",A[i][j]);
-        printf(" :  %+.1e \n",B[i]); }
-}
-
-void femFullSystemPrintInfos(femFullSystem *mySystem)
-{
-    int  size = mySystem->size;
-    printf(" \n");
-    printf("    Full Gaussian elimination \n");
-    printf("    Storage informations \n");
-    printf("    Matrix size      : %8d\n",size);
-    printf("    Bytes required   : %8d\n",(int)sizeof(double)*size*(size+1));     
-}
-
-void  femFullSystemConstrain(femFullSystem *mySystem, int myNode, double myValue) 
-{
-    double  **A, *B;
-    int     i, size;
-    
-    A    = mySystem->A;
-    B    = mySystem->B;
-    size = mySystem->size;
-    
-    for (i=0; i < size; i++) {
-        B[i] -= myValue * A[i][myNode];
-        A[i][myNode] = 0; }
-    
-    for (i=0; i < size; i++) 
-        A[myNode][i] = 0; 
-    
-    A[myNode][myNode] = 1;
-    B[myNode] = myValue;
-}
-
-void femFullSystemAssemble(femFullSystem* mySystem, double *Aloc, double *Bloc, int *map, int nLoc)
-{
-    int i,j;
-    for (i = 0; i < nLoc; i++) { 
-        for(j = 0; j < nLoc; j++) {
-            mySystem->A[map[i]][map[j]] += Aloc[i*nLoc+j]; }
-    mySystem->B[map[i]] += Bloc[i]; }
-}
-
-double* femFullSystemEliminate(femFullSystem *mySystem)
-{
-    double  **A, *B, factor;
-    int     i, j, k, size;
-    
-    A    = mySystem->A;
-    B    = mySystem->B;
-    size = mySystem->size;
-    
-    /* Gauss elimination */
-    
-    for (k=0; k < size; k++) {
-        if ( fabs(A[k][k]) <= 1e-8 ) {
-            printf("Pivot index %d  ",k);
-            printf("Pivot value %e  ",A[k][k]);
-            Error("Cannot eliminate with such a pivot"); }
-        for (i = k+1 ; i <  size; i++) {
-            factor = A[i][k] / A[k][k];
-            for (j = k+1 ; j < size; j++) 
-                A[i][j] = A[i][j] - A[k][j] * factor;
-            B[i] = B[i] - B[k] * factor; }}
-    
-    /* Back-substitution */
-    
-    for (i = size-1; i >= 0 ; i--) {
-        factor = 0;
-        for (j = i+1 ; j < size; j++)
-            factor += A[i][j] * B[j];
-        B[i] = ( B[i] - factor)/A[i][i]; }
-    
-    return(mySystem->B);    
-}
-
-femBandSystem *femBandSystemCreate(int size, int band)
-{
-    femBandSystem *myBandSystem = malloc(sizeof(femBandSystem));
-    myBandSystem->B = malloc(sizeof(double)*size*(band+1));
-    myBandSystem->A = malloc(sizeof(double*)*size);        
-    myBandSystem->size = size;
-    myBandSystem->band = band;
-    myBandSystem->A[0] = myBandSystem->B + size;
-    int i;
-    for (i=1 ; i < size ; i++) 
-        myBandSystem->A[i] = myBandSystem->A[i-1] + band - 1;
-    femBandSystemInit(myBandSystem);
-    return(myBandSystem);
-}
- 
-void femBandSystemFree(femBandSystem *myBandSystem)
-{
-    free(myBandSystem->B);
-    free(myBandSystem->A); 
-    free(myBandSystem);
-}
- 
-void femBandSystemInit(femBandSystem *myBandSystem)
-{
-    int i;
-    int size = myBandSystem->size;
-    int band = myBandSystem->band;
-    for (i=0 ; i < size*(band+1) ; i++) 
-        myBandSystem->B[i] = 0;        
-}
- 
-void femBandSystemPrint(femBandSystem *myBand)
-{
-    double  **A, *B;
-    int     i, j, band, size;
-    A    = myBand->A;
-    B    = myBand->B;
-    size = myBand->size;
-    band = myBand->band;
-
-    for (i=0; i < size; i++) {
-        for (j=i; j < i+band; j++)
-            if (A[i][j] == 0) printf("         ");   
-            else              printf(" %+.1e",A[i][j]);
-        printf(" :  %+.1e \n",B[i]); }
-}
-  
-void femBandSystemPrintInfos(femBandSystem *myBand)
-{
-    int size = myBand->size;
-    int band = myBand->band;
-    printf(" \n");
-    printf("    Banded Gaussian elimination \n");
-    printf("    Storage informations \n");
-    printf("    Matrix size      : %8d\n",size);
-    printf("    Matrix band      : %8d\n",band);
-    printf("    Bytes required   : %8d\n",(int)sizeof(double)*size*(band+1));     
-}
-
-
-double femBandSystemGet(femBandSystem* myBandSystem, int myRow, int myCol)
-{
-    double value = 0;
-    if (myCol >= myRow && myCol < myRow+myBandSystem->band)  value = myBandSystem->A[myRow][myCol]; 
-    return(value);
-}
-
-double femFullSystemGet(femFullSystem* myFullSystem, int myRow, int myCol)
-{
-    return(myFullSystem->A[myRow][myCol]); 
-}
-
-void femBandSystemAssemble(femBandSystem* myBandSystem, double *Aloc, double *Bloc, int *map, int nLoc)
-{
-    int i,j;
-    for (i = 0; i < nLoc; i++) { 
-        int myRow = map[i];
-        for(j = 0; j < nLoc; j++) {
-            int myCol = map[j];
-            if (myCol >= myRow)  myBandSystem->A[myRow][myCol] += Aloc[i*nLoc+j]; }
-        myBandSystem->B[myRow] += Bloc[i]; }
-}
-
-
- 
-void femBandSystemConstrain(femBandSystem *myBand, int myNode, double myValue) 
-{
-    double  **A, *B;
-    int     i, size, band, ifirst, iend;
-    A    = myBand->A;
-    B    = myBand->B;
-    size = myBand->size;
-    band = myBand->band;
-
-    ifirst = fmax(0,myNode - band + 1);
-    iend   = myNode;
-    for (i=ifirst; i < iend; i++) {
-        B[i] -= myValue * A[i][myNode];
-        A[i][myNode] = 0; }
-
-    ifirst = myNode+1;
-    iend = fmin(myNode + band,size);
-    for (i=ifirst; i < iend; i++) {
-        B[i] -= myValue * A[myNode][i];
-        A[myNode][i] = 0; }
-        
-    A[myNode][myNode] = 1;
-    B[myNode] = myValue;
-}
- 
-double  *femBandSystemEliminate(femBandSystem *myBand)
-{
-    double  **A, *B, factor;
-    int     i, j, k, jend, size, band;
-    A    = myBand->A;
-    B    = myBand->B;
-    size = myBand->size;
-    band = myBand->band;
-
-    /* Incomplete Cholesky factorization */ 
-
-    for (k=0; k < size; k++) {
-        if ( fabs(A[k][k]) <= 1e-4 ) {
-            Error("Cannot eleminate with such a pivot"); }
-        jend = fmin(k + band,size);
-        for (i = k+1 ; i <  jend; i++) {
-            factor = A[k][i] / A[k][k];
-            for (j = i ; j < jend; j++) 
-                A[i][j] = A[i][j] - A[k][j] * factor;
-            B[i] = B[i] - B[k] * factor; }}
-        
-    /* Back-substitution */
-
-    for (i = (size-1); i >= 0 ; i--) {
-        factor = 0;
-        jend = fmin(i + band,size);
-        for (j = i+1 ; j < jend; j++)
-            factor += A[i][j] * B[j];
-        B[i] = ( B[i] - factor)/A[i][i]; }
-
-    return(myBand->B);
-}
-
-
 
 ////// ERROR //////
 
